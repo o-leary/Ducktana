02955e4dd8e7c5042e0c55e869d3865b27c0d270
Sean O'Leary <sean@o-leary.co.nz>
2015-08-03 04:44:18 +1200

    Added ability to check if already installed, and inline install, from https://o-leary.com/

--- duckduckgowithcortana.js
+++ duckduckgowithcortana.js
@@ -1,6 +1,18 @@
+// Listener to redirect to Google
 chrome.webRequest.onBeforeRequest.addListener(
 function(details){
-
 	return { redirectUrl: details.url.replace("www.bing.com/search", "duckduckgo.com/")};
-},
- {urls: ["*://www.bing.com/search*"]}, ["blocking"]);
+}, { urls: ["*://www.bing.com/search*"] }, ["blocking"]);
+
+// Listener to check if extension is installed (when browsing installer page)
+chrome.runtime.onMessageExternal.addListener(
+function (request, sender, sendResponse) {
+    if (request) {
+        if (request.message) {
+            if (request.message == "version") {
+                sendResponse({ version: "1.1" });
+            }
+        }
+    }
+    return true;
+});
